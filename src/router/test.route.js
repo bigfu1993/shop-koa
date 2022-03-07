14bce600df6c38db4f0ce2e447b25fe41fcefc8e
bigfu <bigfu1993@163.com>
2022-03-07 15:15:53 +0800

    test centOS

--- src/router/test.route.js
+++ src/router/test.route.js
@@ -4,7 +4,7 @@ const router = new Router({ prefix: '/test' })
 router.get('/', (ctx, next) => {
     ctx.body = {
         code: 0,
-        message: 'get请求成功',
+        message: 'get请求成功,abc',
         result: ''
     }
 })
