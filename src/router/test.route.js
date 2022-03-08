ec6fcc9fa63dcf70c05ef3a3257de647b6f04c09
bigfu <bigfu1993@163.com>
2022-03-08 17:34:00 +0800

    update test.route

--- src/router/test.route.js
+++ src/router/test.route.js
@@ -1,11 +1,15 @@
 const Router = require('koa-router')
 const router = new Router({ prefix: '/test' })
 
-router.get('/', (ctx, next) => {
-    ctx.body = {
-        code: 0,
-        message: 'get请求成功,abc',
-        result: ''
+router.get('/', async (ctx, next) => {
+    try {
+        ctx.body = {
+            code: 0,
+            message: `hello koa!`,
+            result: ''
+        }
+    } catch (error) {
+        console.error('error', error)
     }
 })
 
